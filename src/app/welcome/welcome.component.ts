ee6ab72dbe518e182fb0fff469362315294b257d
stilqnwoke <debelakoto@gmail.com>
2022-01-12 20:46:00 +0200

    welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,1 +1,22 @@
+import { Router, ActivatedRoute } from "@angular/router";
+import { AppComponent } from "./../app.component";
+import { Component, OnInit } from "@angular/core";
 
+@Component({
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.css"],
+})
+export class WelcomeComponent implements OnInit {
+  message = "Welcome message";
+  name = "";
+
+  //ActivatedRouter
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.name = this.route.snapshot.params["name"];
+  }
+}
+
+export class Class1 {}
